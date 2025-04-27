c427f653b7f13b55188d4f32f1ec08b1f937514c
zerohowl <seanesla1156@gmail.com>
2025-04-27 04:56:08 -0700

    fix: Update prebuild script to use ES modules syntax

--- prebuild.js
+++ prebuild.js
@@ -1,5 +1,10 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Get __dirname equivalent in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Create or update .npmrc to force legacy peer deps
 const npmrcPath = path.join(__dirname, '.npmrc');
